42940a38e0fb502e0d5327d0bd1a4217e9db419d
Baptiste Clavié <clavie.b@gmail.com>
2016-02-08 14:29:15 +0100

    Register the event listener pass
    Otherwise, we can't listen to anything which can be kinda bothersome
    (just a little bit, as nothing is working on the app frontend things)

--- src/BareApp.php
+++ src/BareApp.php
@@ -2,11 +2,22 @@
 namespace Bare;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 
 use Bare\DependencyInjection\BareExtension;
 
 class BareApp extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterListenersPass, PassConfig::TYPE_BEFORE_REMOVING);
+    }
+
     /** {@inheritDoc} */
     public function getContainerExtension()
     {
