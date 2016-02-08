dd5038e75f62da255bc95609362edb9d44595bb6
Baptiste Clavié <clavie.b@gmail.com>
2016-02-08 14:32:50 +0100

    Add front controllers

---
+++ web/prod.php
@@ -0,0 +1,17 @@
+<?php
+namespace Bare;
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
+
